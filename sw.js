9ae450a590779b45dc31d064509df51b0605a8bc
Darja Ballarinova <darja.ballarinova@gmail.com>
2018-08-01 15:43:08 +0200

    Add ignoreSearch property to match() in fetch

--- sw.js
+++ sw.js
@@ -25,7 +25,7 @@ self.addEventListener('install', function(event) {
 
 
 self.addEventListener('fetch', function(event) {
-    event.respondWith(caches.match(event.request).then(function(response) {
+    event.respondWith(caches.match(event.request, {ignoreSearch:true}).then(function(response) {
         if(response) {
             return response
         };
